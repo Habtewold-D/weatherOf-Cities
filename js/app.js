b740afc3c5847d84bf25eb869a2c82403f9da96f
Habtewold-D <habtewold21degfie@gmail.com>
2024-11-12 17:03:12 -0600

    can show weather of city

--- js/app.js
+++ js/app.js
@@ -6,17 +6,28 @@ const searchBtn = document.querySelector('.search-box button');
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-
-    var data = await response.json();
+    if (response.status == 404){
+        document.querySelector('.error').style.display = "block";
+        document.querySelector('.weather').style.display = "none";
+    }else{
+        var data = await response.json();
 
     console.log(data);
+
+    document.querySelector('.error').style.display = "none";
+    document.querySelector('.weather').style.display = "block";
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "&deg;c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+    
+    }
 }
 
 searchBtn.addEventListener('click', ()=>{
     checkWeather(searchBox.value);
 });
+
+
+    
